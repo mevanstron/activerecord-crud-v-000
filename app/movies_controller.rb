ac0b5cec9c6ae8b1bf0e699e04dc043be36283a1
Michael Evans <michael@michaelgregoryevans.com>
2017-11-28 15:53:19 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(title:)
+  movie = Movie.new(:title)
 end
 
 def can_be_created_in_a_block
